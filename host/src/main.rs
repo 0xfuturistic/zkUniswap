12542f347adfa10d17d5cd14070dc6bd7c65cb4e
Diego <105765223+0xfuturistic@users.noreply.github.com>
2023-09-09 18:33:32 +0200

    read values for variables in guest code from environment

--- host/src/main.rs
+++ host/src/main.rs
@@ -1,21 +1,26 @@
 // TODO: Update the name of the method loaded by the prover. E.g., if the method
 // is `multiply`, replace `METHOD_NAME_ELF` with `MULTIPLY_ELF` and replace
 // `METHOD_NAME_ID` with `MULTIPLY_ID`
+use ethers_core::types::{I256, U256};
 use methods::{METHOD_NAME_ELF, METHOD_NAME_ID};
-use risc0_zkvm::{default_prover, ExecutorEnv};
+use risc0_zkvm::{
+    default_prover,
+    serde::{from_slice, to_vec},
+    ExecutorEnv, Receipt,
+};
 
 fn main() {
-    // First, we construct an executor environment
-    let env = ExecutorEnv::builder().build().unwrap();
+    let price = U256::from_dec_str("79228162514264337593543950336").unwrap();
+    let price_target = U256::from_dec_str("79623317895830914510639640423").unwrap();
+    let amount = I256::from_dec_str("1000000000000000000").unwrap();
 
-    // TODO: add guest input to the executor environment using
-    // ExecutorEnvBuilder::add_input().
-    // To access this method, you'll need to use the alternate construction
-    // ExecutorEnv::builder(), which creates an ExecutorEnvBuilder. When you're
-    // done adding input, call ExecutorEnvBuilder::build().
-
-    // For example:
-    // let env = ExecutorEnv::builder().add_input(&vec).build().unwrap();
+    let env = ExecutorEnv::builder()
+        // Send a & b to the guest
+        .add_input(&to_vec(&price).unwrap())
+        .add_input(&to_vec(&price_target).unwrap())
+        .add_input(&to_vec(&amount).unwrap())
+        .build()
+        .unwrap();
 
     // Obtain the default prover.
     let prover = default_prover();
@@ -29,4 +34,12 @@ fn main() {
     // Optional: Verify receipt to confirm that recipients will also be able to
     // verify your receipt
     receipt.verify(METHOD_NAME_ID).unwrap();
+
+    // Extract journal of receipt (i.e. output c, where c = a * b)
+    let c: U256 = from_slice(&receipt.journal).expect(
+        "Journal output should deserialize into the same types (& order) that it was written",
+    );
+
+    // Report the product
+    println!("The amount out is {}, and I can prove it!", c);
 }
--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -1,20 +1,20 @@
 #![no_main]
 
-
-use risc0_zkvm::guest::env;
-use uniswap_v3_math::{swap_math::compute_swap_step, sqrt_price_math::{get_next_sqrt_price_from_input, get_next_sqrt_price_from_output}};
 use ethers_core::types::{I256, U256};
+use risc0_zkvm::guest::env;
+use uniswap_v3_math::swap_math::compute_swap_step;
 
 risc0_zkvm::guest::entry!(main);
 
 pub fn main() {
-    let price = U256::from_dec_str("79228162514264337593543950336").unwrap();
-    let price_target = U256::from_dec_str("79623317895830914510639640423").unwrap();
+    let price = env::read();
+    let price_target = env::read();
     let liquidity = 2e18 as u128;
-    let amount = I256::from_dec_str("1000000000000000000").unwrap();
+    let amount = env::read();
     let fee = 600;
-    let zero_for_one = false;
 
     let (sqrt_p, amount_in, amount_out, fee_amount) =
         compute_swap_step(price, price_target, liquidity, amount, fee).unwrap();
+
+    env::commit(&amount_out);
 }
