5f11532dc29d84ee1283e96e04b3d3e7deabb737
Diego <105765223+0xfuturistic@users.noreply.github.com>
2023-09-09 18:07:37 +0200

    add sample computation in guest for swap

--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -2,9 +2,19 @@
 
 
 use risc0_zkvm::guest::env;
+use uniswap_v3_math::{swap_math::compute_swap_step, sqrt_price_math::{get_next_sqrt_price_from_input, get_next_sqrt_price_from_output}};
+use ethers_core::types::{I256, U256};
 
 risc0_zkvm::guest::entry!(main);
 
 pub fn main() {
-    // TODO: Implement your guest code here
+    let price = U256::from_dec_str("79228162514264337593543950336").unwrap();
+    let price_target = U256::from_dec_str("79623317895830914510639640423").unwrap();
+    let liquidity = 2e18 as u128;
+    let amount = I256::from_dec_str("1000000000000000000").unwrap();
+    let fee = 600;
+    let zero_for_one = false;
+
+    let (sqrt_p, amount_in, amount_out, fee_amount) =
+        compute_swap_step(price, price_target, liquidity, amount, fee).unwrap();
 }
